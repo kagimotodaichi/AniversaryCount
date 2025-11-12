7b83a7b6a5200f649f92085d1dc578e3cfb79f6c
daichi kagimoto <daichi.kagimoto@icloud.com>
2025-11-12 22:30:01 +0900

    Initial Commit

---
+++ AniversaryCount/AniversaryCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  AniversaryCountApp.swift
+//  AniversaryCount
+//
+//  Created by 鍵本大地 on 2025/11/12.
+//
+
+import SwiftUI
+
+@main
+struct AniversaryCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AniversaryCount/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AniversaryCount
+//
+//  Created by 鍵本大地 on 2025/11/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
